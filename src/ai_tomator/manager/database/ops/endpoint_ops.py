aa2c6b658b1f9f423d84ffe33003711379c4dd96
Ben <benjino@posteo.de>
2025-11-10 23:19:25 +0100

    feat(database): add ops

---
+++ src/ai_tomator/manager/database/ops/endpoint_ops.py
@@ -0,0 +1,41 @@
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
+from ai_tomator.core.exceptions import NameAlreadyExistsError
+from ai_tomator.manager.database.models.endpoint import Endpoint
+
+
+class EndpointOps:
+    def __init__(self, session_local: sessionmaker):
+        self.SessionLocal = session_local
+
+    def add(self, name: str, engine: str, url=None, token=None):
+        with self.SessionLocal() as session:
+            ep = Endpoint(name=name, engine=engine, url=url, token=token)
+            session.add(ep)
+            try:
+                session.commit()
+            except IntegrityError:
+                session.rollback()
+                raise NameAlreadyExistsError(name)
+
+    def get(self, name: str, show_api=False):
+        with self.SessionLocal() as session:
+            ep = session.query(Endpoint).filter_by(name=name).first()
+            if not ep:
+                raise ValueError(f"Endpoint '{name}' not found.")
+            if show_api:
+                return ep.to_dict_internal()
+            else:
+                return ep.to_dict_public()
+
+    def list(self):
+        with self.SessionLocal() as session:
+            return [e.to_dict_public() for e in session.query(Endpoint).all()]
+
+    def delete(self, name: str):
+        with self.SessionLocal() as session:
+            ep = session.query(Endpoint).filter_by(name=name).first()
+            if not ep:
+                raise ValueError(f"Endpoint '{name}' not found.")
+            session.delete(ep)
+            session.commit()
---
+++ src/ai_tomator/manager/database/ops/result_ops.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import sessionmaker
+from ai_tomator.manager.database.models.result import Result, Batch, File
+
+
+class ResultOps:
+    def __init__(self, session_local: sessionmaker):
+        self.SessionLocal = session_local
+
+    def save(self, batch_id: int, file_name: str, input: str, output: str):
+        with self.SessionLocal() as session:
+            batch = session.query(Batch).filter_by(id=batch_id).first()
+            if not batch:
+                raise ValueError(f"Batch with id '{id}' not found.")
+            file_id = session.query(File).filter_by(storage_name=file_name).first().id
+
+            session.add(Result(
+                batch_id=batch_id,
+                file_id=file_id,
+                file_name=file_name,
+                input=input,
+                output=output,
+                engine=batch.engine,
+                endpoint=batch.endpoint,
+                file_reader=batch.file_reader,
+                prompt=batch.prompt,
+                model=batch.model,
+                temperature=batch.temperature
+            ))
+            session.commit()
+
+    def list_by_batch(self, batch_id):
+        with self.SessionLocal() as session:
+            rows = session.query(Result).filter_by(batch_id=batch_id).all()
+            return [r.to_dict() for r in rows]
