1689d569f48a054d6bde932345349e7def96de9e
Ben <benjino@posteo.de>
2025-11-10 23:24:11 +0100

    feat(core): add custom exceptions

---
+++ src/ai_tomator/core/exceptions.py
@@ -0,0 +1,7 @@
+class NameAlreadyExistsError(Exception):
+    """Raised when a given name already exists."""
+    pass
+
+class ResourceExistsError(Exception):
+    """Raised when a resource already exists."""
+    pass
