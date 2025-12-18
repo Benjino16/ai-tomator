de615dbb925965d42e01ebb22de0088dc21505e1
Ben <benjino@posteo.de>
2025-12-18 11:16:12 +0100

    feat(webui): add batch services for caching

--- src/ai_tomator/webui/static/js/service/index.js
+++ src/ai_tomator/webui/static/js/service/index.js
@@ -1,6 +1,11 @@
 import { createFilesService } from "./file_service.js";
+import {createBatchService} from "./batch_service.js";
 import { API } from "../api/index.js";
+import {createEndpointService} from "./endpoint_service.js";
+
 
 export const Service = {
     Files: createFilesService(API),
+    Batches: createBatchService(API),
+    Endpoints: createEndpointService(API),
 };
